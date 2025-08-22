470e52fac74861afd67884f14af725e3696b46d0
Lickhill <likhilnm17103@gmail.com>
2025-08-21 22:38:45 -0700

    added ad sense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -85,6 +86,13 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
+				<Script
+					id="adsense-script"
+					async
+					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5475289645274873"
+					crossOrigin="anonymous"
+					strategy="afterInteractive"
+				/>
 				{children}
 				<Analytics />
 				<SpeedInsights />
